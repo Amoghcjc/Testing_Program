cbc61e0b5a00de71ae3d8a4ac82193fc9d54fddd
Lenovo <Lenovo@Amogh>
2023-05-16 12:24:21 +0530

    By Prafull

---
+++ git_program/src/git_programcom/demo/B.java
@@ -0,0 +1,20 @@
+package git_programcom.demo;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Create a M1 Method");
+	}
+	
+		public void m1()
+		{
+			System.out.println("M1_____Method");
+			
+		}
+		
+		public void m2()
+		{
+			System.out.println("M2_____Method");
+		}
+	}
