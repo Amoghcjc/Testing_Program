889764b34f107f1242491564316b5b433c112343
Lenovo <Lenovo@Amogh>
2023-05-16 11:13:58 +0530

    ByAmogh

--- git_program/src/git_programcom/demo/A.java
+++ git_program/src/git_programcom/demo/A.java
@@ -2,11 +2,31 @@ package git_programcom.demo;
 
 public class A 
 {
+	int a;
+	int b;
+	int x;
+	int y;
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
 		System.out.println("Hello");
 		System.out.println("Hello");
+	}
+	public void m1()
+	{
+		System.out.println("Addition for 2 numbers");
+		
+		int c=a+b;
+		
+		System.out.println("Addition Of 2 Numbers="+c);
+	}
+	public void m2()
+	{
+		System.out.println("Substraction for 2 numbers");
+		
+		int z=x+y;
+		
+		System.out.println("Substraction of 2 Numbers="+z);
 	}
 
 }
