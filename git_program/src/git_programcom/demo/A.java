1f9a24b0a9b89f3d0ec290ed897841e674f53949
Amogh <133480707+Amoghcjc@users.noreply.github.com>
2023-05-16 11:18:25 +0530

    ByAmit
    Add two Methods for Substraction and Multiplication

--- git_program/src/git_programcom/demo/A.java
+++ git_program/src/git_programcom/demo/A.java
@@ -28,5 +28,22 @@ public class A
 		
 		System.out.println("Substraction of 2 Numbers="+z);
 	}
+	public void m3()
+	{
+		System.out.println("multiplication for 2 numbers");
+		
+		int c=a*b;
+		
+		System.out.println("Multiplication Of 2 Numbers="+c);
+	}
+	public void m4()
+	{
+		System.out.println("Substraction for 2 numbers");
+		
+		int z=x/y;
+		
+		System.out.println("Substraction of 2 Numbers="+z);
+	}
+	
 
 }
